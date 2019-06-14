f863a3f9e2e7ae42040e9a1670b844a61b0ef569
PP <chinese1236@qq.com>
2019-06-14 11:16:30 +0800

    move in xiaomi platform related code (#9)
    * move in engine platform related code
    
    * update code

--- xiaomigame/adapter/engine/Game.js
+++ xiaomigame/adapter/engine/Game.js
@@ -37,11 +37,10 @@ Object.assign(game, {
         // Avoid setup to be called twice.
         if (this._rendererInitialized) return;
 
-        let globalCanvas = canvas;
+        let localCanvas = this.canvas = canvas;
 
         this.container = document.createElement("DIV");
         this.frame = this.container.parentNode === document.body ? document.documentElement : this.container.parentNode;
-        this.canvas = globalCanvas;
 
         this._determineRenderType();
         // WebGL context created successfully
@@ -50,10 +49,10 @@ Object.assign(game, {
                 'stencil': true,
                 // MSAA is causing serious performance dropdown on some browsers.
                 'antialias': cc.macro.ENABLE_WEBGL_ANTIALIAS,
-                'alpha': cc.macro.ENABLE_TRANSPARENT_CANVAS
+                'alpha': cc.macro.ENABLE_TRANSPARENT_CANVAS,
+                'preserveDrawingBuffer': false,
             };
-            opts['preserveDrawingBuffer'] = true;
-            renderer.initWebGL(globalCanvas, opts);
+            renderer.initWebGL(localCanvas, opts);
             this._renderContext = renderer.device._gl;
             
             // Enable dynamic atlas manager by default
@@ -64,88 +63,29 @@ Object.assign(game, {
         if (!this._renderContext) {
             this.renderType = this.RENDER_TYPE_CANVAS;
             // Could be ignored by module settings
-            renderer.initCanvas(globalCanvas);
+            renderer.initCanvas(localCanvas);
             this._renderContext = renderer.device._ctx;
         }
-
-        this.canvas.oncontextmenu = function () {
-            if (!cc._isContextMenuEnable) return false;
-        };
 
         this._rendererInitialized = true;
     },
 
     _initEvents () {
-        var win = window, hiddenPropName;
-
         // register system events
-        if (this.config.registerSystemEvent)
+        if (this.config.registerSystemEvent) {
             inputManager.registerSystemEvent(this.canvas);
-
-        if (typeof document.hidden !== 'undefined') {
-            hiddenPropName = "hidden";
-        } else if (typeof document.mozHidden !== 'undefined') {
-            hiddenPropName = "mozHidden";
-        } else if (typeof document.msHidden !== 'undefined') {
-            hiddenPropName = "msHidden";
-        } else if (typeof document.webkitHidden !== 'undefined') {
-            hiddenPropName = "webkitHidden";
         }
-
-        var hidden = false;
 
         function onHidden () {
-            if (!hidden) {
-                hidden = true;
-                game.emit(game.EVENT_HIDE);
-            }
-        }
-        // In order to adapt the most of platforms the onshow API.
-        function onShown (arg0, arg1, arg2, arg3, arg4) {
-            if (hidden) {
-                hidden = false;
-                game.emit(game.EVENT_SHOW, arg0, arg1, arg2, arg3, arg4);
-            }
+            game.emit(game.EVENT_HIDE);
         }
 
-        if (hiddenPropName) {
-            var changeList = [
-                "visibilitychange",
-                "mozvisibilitychange",
-                "msvisibilitychange",
-                "webkitvisibilitychange",
-                "qbrowserVisibilityChange"
-            ];
-            for (var i = 0; i < changeList.length; i++) {
-                document.addEventListener(changeList[i], function (event) {
-                    var visible = document[hiddenPropName];
-                    // QQ App
-                    visible = visible || event["hidden"];
-                    if (visible)
-                        onHidden();
-                    else
-                        onShown();
-                });
-            }
-        } else {
-            win.addEventListener("blur", onHidden);
-            win.addEventListener("focus", onShown);
-        }
-
-        if (win.navigator.userAgent.indexOf("MicroMessenger") > -1) {
-            win.onfocus = onShown;
+        function onShown (res) {
+            game.emit(game.EVENT_SHOW, res);
         }
 
         qg.onShow && qg.onShow(onShown);
         qg.onHide && qg.onHide(onHidden);
-
-        if ("onpageshow" in window && "onpagehide" in window) {
-            win.addEventListener("pagehide", onHidden);
-            win.addEventListener("pageshow", onShown);
-            // Taobao UIWebKit
-            document.addEventListener("pagehide", onHidden);
-            document.addEventListener("pageshow", onShown);
-        }
 
         this.on(game.EVENT_HIDE, function () {
             game.pause();
